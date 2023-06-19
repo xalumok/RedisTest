e7d2ea72520599b26d9dd91bebb193524cec6004
Vladislav <vladislavz@cynet.com>
2023-06-19 16:47:38 +0300

    add redis test

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+import random
+from asyncio import run
+import redis 
+
+class RedisTest:
+    def __init__(self) -> None:
+        self.time = time.time()
+        self.name = f"Instance{random.randint(9,99)}"
+
+async def main():
+    # test = RedisTest()
+    # print(f'Data: {test.name}')
+
+    redis_client = redis.Redis(host='127.0.0.1',port=6379,decode_responses=True)
+    redis_client.publish('Creep',f'yura loh at {time.time()}')
+
+    redis_sub = redis_client.pubsub()
+    redis_sub.subscribe('Creep')
+
+    for message in redis_sub.listen():
+        print(f"Got data: {message} at {time.time()}")
+
+run(main())
