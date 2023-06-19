0e0aac7f4331e9a28c54aa14654c2639abbdabba
Vladislav <vladislavz@cynet.com>
2023-06-19 16:54:13 +0300

    logs added

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ async def main():
     # print(f'Data: {test.name}')
 
     redis_client = redis.Redis(host='127.0.0.1',port=6379,decode_responses=True)
-    redis_client.publish('Creep',f'yura loh at {time.time()}')
+    now = time.time()
+    redis_client.publish('Creep2',f'yura loh at {now}')
+    print(f'Sent data at: {now}')
 
     redis_sub = redis_client.pubsub()
     redis_sub.subscribe('Creep')
